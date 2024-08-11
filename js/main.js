3c873c0aaafe4fd6616075661e86f678c3713ff6
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-08-11 11:50:17 +0500

    media menu in progress

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// catalog
+let catalog_btn = document.querySelector('.catalog_btn');
+let catalog = document.querySelector('.catalog');
+
+catalog_btn.addEventListener('click', () => {
+  catalog.classList.toggle('active');
+})
+// catalog
