89892d496a2066e49a516942c373f9407e538107
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-08-14 13:32:48 +0500

    catalog the end

--- js/main.js
+++ js/main.js
@@ -162,36 +162,30 @@ $('.vodiapicker option').each(function(){
 
 $('#a').html(langArray);
 
-//Set the button value to the first el of the array
 $('.btn-select').html(langArray[0]);
 $('.btn-select').attr('value', 'en');
 
-//change button stuff on click
 $('#a li').click(function(){
-   var img = $(this).find('img').attr("src");
-   var value = $(this).find('img').attr('value');
-   var text = this.innerText;
-   var item = '<li><img src="'+ img +'" alt="" /><span>'+ text +'</span></li>';
+  var img = $(this).find('img').attr("src");
+  var value = $(this).find('img').attr('value');
+  var text = this.innerText;
+  var item = '<li><img src="'+ img +'" alt="" /><span>'+ text +'</span></li>';
   $('.btn-select').html(item);
   $('.btn-select').attr('value', value);
   $(".b").toggle();
-  //console.log(value);
 });
 
 $(".btn-select").click(function(){
-        $(".b").toggle();
+        // $(".b").toggle();
     });
 
-//check local storage for the lang
 var sessionLang = localStorage.getItem('lang');
 if (sessionLang){
-  //find an item with value of sessionLang
   var langIndex = langArray.indexOf(sessionLang);
   $('.btn-select').html(langArray[langIndex]);
   $('.btn-select').attr('value', sessionLang);
 } else {
-   var langIndex = langArray.indexOf('ch');
+    var langIndex = langArray.indexOf('ch');
   console.log(langIndex);
   $('.btn-select').html(langArray[langIndex]);
-  //$('.btn-select').attr('value', 'en');
 }
